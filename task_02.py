db4893230bd82d8229f44833ade2a6bef93a5914
xiangyq <youqing.xiang@spsmail.cuny.edu>
2015-02-10 06:03:24 +0000

    task_02.py again

--- task_02.py
+++ task_02.py
@@ -9,4 +9,4 @@ SPANISH = inquisition.SPANISH
 I = SPANISH.index('Spanish', 1)
 N = len('Spanish')
 L = len(SPANISH)
-FISHY = SPANISH[0:I] + 'Flemish' + SPANISH[I+N:L]
+FLEMISH = SPANISH[0:I] + 'Flemish' + SPANISH[I+N:L]
