e4f81980cd5fa2a5bba0424282697107aff096b9
xiangyq <youqing.xiang@spsmail.cuny.edu>
2015-02-10 06:18:02 +0000

    task_05.py again

--- task_05.py
+++ task_05.py
@@ -26,7 +26,7 @@ def get_member_count(my_sequence):
     try:
         length = len(my_sequence)
     except TypeError:
-        length = 0
+        length = NULL
 
     return length
 
@@ -63,8 +63,10 @@ def is_empty(my_sequence):
 
     if count != 0:
         return False
-    else:
-        return True
+    elif count = 0:
+        return True        
+    elif count = NULL
+        return 'TypeError: oject has no len()'
 
 
 TEST = ''
